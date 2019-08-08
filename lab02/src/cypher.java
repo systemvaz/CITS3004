ea27154df78d0ff8c2e43667c5de674f5ff2dadb
systemvaz <systemvaz@outlook.com>
2019-08-08 20:11:12 +0800

    update

--- lab02/src/cypher.java
+++ lab02/src/cypher.java
@@ -4,8 +4,6 @@ public class cypher
 	{
 		int i = 0;
 		char replace;
-		
-		//text.toLowerCase();
 		char[] ourarray = text.toCharArray();
 		char[] newarray = new char[text.length()];
 		
@@ -13,13 +11,13 @@ public class cypher
 		{
 			if(c != ' ')
 			{
-				replace = (char) ((int)c - key);
+				replace = ((char)((int)c - key));
 				newarray[i] = replace;
 			}
 			i++;
 		}
 		
-		String result = String.copyValueOf(newarray);
+		String result = newarray.toString();
 		return result;
 	}
 	
@@ -28,7 +26,7 @@ public class cypher
 	{
 		String thetext = "Ihwy ojih u ncgynbyly qum u ahigy hugyx Jynyl. By qum hin u vlcabn ahigy von by ehyq nbun ihy xus by qcff vywigy nby alyunymn ahigy ypyl ehiqh. Ohzilnohunyfs, by qum zuwyx qcnb u alyun xuhayl qbyh bcm zlcyhx Viv xywcxyx nbun by qcff vy nby vymn ahigy ypyl ehiqh. Jynyl xcx hin quhn ni nolh uauchmn bcm zlcyhx Viv von by qum fyzn qcnb hi wbicwy von ni ai uauchmn bcg";
 		
-		for(int i = 1; i < 27; i++)
+		for(int i = 0; i < 27; i++)
 		{
 			System.out.println(caesar(thetext, i));	
 		}
