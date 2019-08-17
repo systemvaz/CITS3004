74b5b1bddd8b926dd460a5e8f8018985d1b44172
systemvaz <systemvaz@outlook.com>
2019-08-17 15:54:52 +0800

    update

--- lab02/src/cypher.java
+++ lab02/src/cypher.java
@@ -77,12 +77,13 @@ public class cypher
 	public static void main(String[] args)
 	{
 		String thetext = "Ihwy ojih u ncgynbyly qum u ahigy hugyx Jynyl. By qum hin u vlcabn ahigy von by ehyq nbun ihy xus by qcff vywigy nby alyunymn ahigy ypyl ehiqh. Ohzilnohunyfs, by qum zuwyx qcnb u alyun xuhayl qbyh bcm zlcyhx Viv xywcxyx nbun by qcff vy nby vymn ahigy ypyl ehiqh. Jynyl xcx hin quhn ni nolh uauchmn bcm zlcyhx Viv von by qum fyzn qcnb hi wbicwy von ni ai uauchmn bcg";
+		String plaintext = "Once upon a timethere was a gnome named Peter. He was not a bright gnome but he knew that one day he will become the greatest gnome ever known. Unfortunately, he was faced with a great danger when his friend Bob decided that he will be the best gnome ever known. Peter did not want to turn against his friend Bob but he was left with no choice but to go against him";
 		
 		for(int i = 0; i < 27; i++)
 		{
 			System.out.println("Key: " + i);
 			String hacked = caesar_decrypt(thetext, i);
-			String encrypt = caesar_encrypt("Onc", i);
+			String encrypt = caesar_encrypt(plaintext, i);
 			System.out.println(hacked);
 			System.out.println(encrypt);
 		}
